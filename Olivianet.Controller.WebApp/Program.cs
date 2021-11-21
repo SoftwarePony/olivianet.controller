c294ad2c942702344c368aefc9b587c05e0cc26b
SoftwarePony <22513463+SoftwarePony@users.noreply.github.com>
2021-11-20 21:10:26 -0800

    Adds Initial Project

---
+++ Olivianet.Controller.WebApp/Program.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Olivianet.Controller.WebApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+        // Register the OpenIddict core components.
+        .AddCore(options =>
+        {
+            // Configure OpenIddict to use the Entity Framework Core stores and models.
+            // Note: call ReplaceDefaultEntities() to replace the default entities.
+            options.UseEntityFrameworkCore()
+                   .UseDbContext<ApplicationDbContext>();
+        })
+        // Register the OpenIddict server components.
+        .AddServer(options =>
+        {
+            // Enable the token endpoint.
+            options.SetTokenEndpointUris("/connect/token");
+
+            // Enable the client credentials flow.
+            options.AllowClientCredentialsFlow();
+
+            // Register the signing and encryption credentials.
+            options.AddDevelopmentEncryptionCertificate()
+                  .AddDevelopmentSigningCertificate();
+
+            // Register the ASP.NET Core host and configure the ASP.NET Core options.
+            options.UseAspNetCore()
+                  .EnableTokenEndpointPassthrough();
+        })
+        // Register the OpenIddict validation components.
+        .AddValidation(options =>
+        {
+            // Import the configuration from the local OpenIddict server instance.
+            options.UseLocalServer();
+
+            // Register the ASP.NET Core host.
+            options.UseAspNetCore();
+        });
+
+        // Register the worker responsible of seeding the database with the sample clients.
+        // Note: in a real world application, this step should be part of a setup script.
+        //builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.UseEndpoints(options =>
+{
+    options.MapControllers();
+    options.MapDefaultControllerRoute();
+});
+
+app.UseWelcomePage();
+
+app.Run();
